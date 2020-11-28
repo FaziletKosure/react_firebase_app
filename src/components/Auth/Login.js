9f33318210bd6944a718c3d421b231f7d43889f1
FaziletKosure <faziletkosure1@gmail.com>
2020-11-29 00:49:52 +0100

    added reusable form hook

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,22 +1,44 @@
 import React, {useState} from "react";
+import useFormValidation from "./useFormValidation";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+};
 
 function Login(props) {
+  const { handleSubmit, handleChange, values } = useFormValidation(
+    INITIAL_STATE
+  );
   const [login,setLogin]=useState(true)
+  // const [name,setName]=useState("")
+  // const [email,setEmail]=useState("")
+  // const [password,setPassword]=useState("")
   return(
      <div>
      <h2 className='mv3'>{login ? "Login" : "Create Account"}</h2>
-     <form className='flex flex-column'>
+     <form onSubmit={handleSubmit} className='flex flex-column'>
       {!login && <input 
+      onChange={handleChange}
+      value={values.name}
        type="text" 
+       name="name"
        placeholder='Your name'
        autoComplete='off'
        />}
         <input 
+        onChange={handleChange}
+        value={values.email}
+        name="email"
        type="email" 
        placeholder='Your Email'
        autoComplete='off'
        />
         <input 
+        onChange={handleChange}
+        value={values.password}
+        name="password"
        type="password" 
        placeholder='Choose a secure password' 
        />
