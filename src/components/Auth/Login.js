a6fbcaec8e03b07e362a18c52588f2155f065d59
FaziletKosure <faziletkosure1@gmail.com>
2020-11-29 13:31:28 +0100

    created firebase project

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
+import React from "react";
 import useFormValidation from "./useFormValidation";
+import validateLogin from "./validateLogin";
 
 const INITIAL_STATE = {
   name: "",
@@ -8,54 +9,71 @@ const INITIAL_STATE = {
 };
 
 function Login(props) {
-  const { handleSubmit, handleChange, values } = useFormValidation(
-    INITIAL_STATE
-  );
-  const [login,setLogin]=useState(true)
-  // const [name,setName]=useState("")
-  // const [email,setEmail]=useState("")
-  // const [password,setPassword]=useState("")
-  return(
-     <div>
-     <h2 className='mv3'>{login ? "Login" : "Create Account"}</h2>
-     <form onSubmit={handleSubmit} className='flex flex-column'>
-      {!login && <input 
-      onChange={handleChange}
-      value={values.name}
-       type="text" 
-       name="name"
-       placeholder='Your name'
-       autoComplete='off'
-       />}
-        <input 
-        onChange={handleChange}
-        value={values.email}
-        name="email"
-       type="email" 
-       placeholder='Your Email'
-       autoComplete='off'
-       />
-        <input 
-        onChange={handleChange}
-        value={values.password}
-        name="password"
-       type="password" 
-       placeholder='Choose a secure password' 
-       />
-       <div className='flex mt3'>
-         <button type='submit' className='button pointer mr2'>
-           Submit
-         </button>
-         <button type='button' className='pointer button'
-         onClick={()=>setLogin(prev=>!prev)}
-         >
-         {login ? "need to create an account?" : "already have an account?"}
-         </button>
+  const {
+    handleSubmit,
+    handleBlur,
+    handleChange,
+    values,
+    errors,
+    isSubmitting
+  } = useFormValidation(INITIAL_STATE, validateLogin);
+  const [login, setLogin] = React.useState(true);
 
-       </div>
-       
-     </form>
-      </div>)
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form onSubmit={handleSubmit} className="flex flex-column">
+        {!login && (
+          <input
+            onChange={handleChange}
+            value={values.name}
+            name="name"
+            type="text"
+            placeholder="Your name"
+            autoComplete="off"
+          />
+        )}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.email}
+          name="email"
+          type="email"
+          className={errors.email && "error-input"}
+          placeholder="Your email"
+          autoComplete="off"
+        />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+          className={errors.password && "error-input"}
+          name="password"
+          type="password"
+          placeholder="Choose a secure password"
+        />
+        {errors.password && <p className="error-text">{errors.password}</p>}
+        <div className="flex mt3">
+          <button
+            type="submit"
+            className="button pointer mr2"
+            disabled={isSubmitting}
+            style={{ background: isSubmitting ? "grey" : "orange" }}
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="pointer button"
+            onClick={() => setLogin(prevLogin => !prevLogin)}
+          >
+            {login ? "need to create an account?" : "already have an account?"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,5 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+import firebaseConfig from './config'
+// Initialize Firebase
+ app.initializeApp(firebaseConfig);
