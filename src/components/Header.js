28104ce5c7293834334cce497662170bb83e4f68
FaziletKosure <faziletkosure1@gmail.com>
2020-11-29 23:29:09 +0100

    useAuth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,48 @@
 import React from "react";
-import {withRouter,NavLink} from 'react-router-dom'
+import { withRouter, NavLink } from "react-router-dom";
+import { FirebaseContext } from "../firebase";
 
 function Header() {
-  return( 
-  <div className='header'>
-      <div className="flex">  
-      <img src="/logo.png" alt="Hooks News Logo"className="logo"/>  
-      <NavLink to='/' className='header-title'>Hooks News</NavLink>
-      <NavLink to='/' className='header-link'>New</NavLink>
-      <div className="divider"></div>
-      <NavLink to='/top' className='header-link'>Top</NavLink>
-      <div className="divider"></div>
-      <NavLink to='/search' className='header-link'>Search</NavLink>
-      <div className="divider"></div>
-      <NavLink to='/creata' className='header-link'>Submit</NavLink>
-      </div> 
+  const { user, firebase } = React.useContext(FirebaseContext);
+
+  return (
+    <div className="header">
       <div className="flex">
-      <NavLink to='/login' className='header-link'>Login</NavLink>
+        <img src="/logo.png" alt="Hooks News Logo" className="logo" />
+        <NavLink to="/" className="header-title">
+          Hooks News
+        </NavLink>
+        <NavLink to="/" className="header-link">
+          new
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/top" className="header-link">
+          top
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/search" className="header-link">
+          search
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/create" className="header-link">
+          submit
+        </NavLink>
       </div>
-    </div>)
+      <div className="flex">
+        {user ? (
+          <>
+            <div className="header-name">{user.displayName}</div>
+            <div className="divider">|</div>
+            <div className="header-button">logout</div>
+          </>
+        ) : (
+          <NavLink to="/login" className="header-link">
+            login
+          </NavLink>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default withRouter( Header);
+export default withRouter(Header);
